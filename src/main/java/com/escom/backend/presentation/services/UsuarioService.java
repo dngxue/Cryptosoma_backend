ded50349b76f41c9531ac6c2f990597cb696ca78
Sergew_RMZ <serge15games@gmail.com>
2025-06-19 01:42:24 -0600

    Usuario obtener corregido

--- src/main/java/com/escom/backend/presentation/services/UsuarioService.java
+++ src/main/java/com/escom/backend/presentation/services/UsuarioService.java
@@ -3,6 +3,7 @@ package com.escom.backend.presentation.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.escom.backend.domain.dto.UsuarioResponseDTO;
 import com.escom.backend.domain.entities.users.Paciente;
 import com.escom.backend.domain.entities.users.Usuario;
 import com.escom.backend.domain.repositories.PacienteRepository;
@@ -13,10 +14,11 @@ public class UsuarioService {
   @Autowired
   private PacienteRepository pacienteRepository;
 
-  public Usuario getUsuarioByMatricula(String matricula) {
+  public UsuarioResponseDTO getUsuarioByMatricula(String matricula) {
     Paciente paciente = pacienteRepository.findByMatricula(matricula)
         .orElseThrow(() -> new RuntimeException("Usuario no encontrado con matrícula: " + matricula));
-  
-    return paciente.getUsuario();
-      }
+    
+    Usuario usuario = paciente.getUsuario();
+    return new UsuarioResponseDTO(paciente.getMatricula(), paciente.getCurp(), usuario.getNombre(), usuario.getFechaNacimiento());
+  }
 }
